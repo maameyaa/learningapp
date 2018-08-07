286f54c496b3ea7d73d100fbce9d5eab3ff219d7
Maame Yaa Afriyie Poku <maameyaa@techloftgh.com>
2018-08-07 13:23:35 +0000

    page to display lessons included

---
+++ src/pages/displaylesson/displaylesson.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DisplaylessonPage } from './displaylesson';
+
+@NgModule({
+  declarations: [
+    DisplaylessonPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DisplaylessonPage),
+  ],
+})
+export class DisplaylessonPageModule {}
